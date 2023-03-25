786b7454c13cf2dfae7cfbdfa56ace3dcfc9645a
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-25 18:40:11 +0900

    useContext - get date from API

--- src/contexts/Index.tsx
+++ src/contexts/Index.tsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Child } from "./Child";
+import { getUserById } from "../api/user";
 import { MainContent } from "./MainContent";
 import { UserContext } from "./UserContext";
 
 export function Index() {
-  const user: UserType = {
-    user_id: 1,
-    name: "hoge",
-    email: "hogehoge@gmail.com",
-  };
+  const [user, setUser] = useState<UserType>();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const u = await getUserById(1);
+      setUser(u);
+    };
+    getUser();
+  }, []);
 
   return (
     <div className="flex flex-col gap-6 bg-slate-900 p-4">
       {/* NOTE: use Context */}
-      <UserContext.Provider value={user}>
-        <h2 className="text-2xl">MainContent - useContext Test</h2>
+      {user && (
+        <UserContext.Provider value={user}>
+          <h2 className="text-2xl">MainContent - useContext Test</h2>
 
-        <MainContent />
-        <Child />
-      </UserContext.Provider>
+          <MainContent />
+          <Child />
+        </UserContext.Provider>
+      )}
     </div>
   );
 }
